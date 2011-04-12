4ff0194a64a403cb3add5e65084cb482871730ab
Markus Gerdau <markus.gerdau@gmail.com>
2011-04-12 12:55:05 +0200

    Work in progress

--- application/Models/productModel.php
+++ application/Models/productModel.php
@@ -6,7 +6,35 @@
 *
 */
 class class_container extends joz_activeRecordModel{
-public $primaryKey = 'id';
-public $tableName = 'pec_products';
 
+	public $primaryKey = 'id';
+	public $tableName = 'pec_products';
+	
+	public function getLinkName()
+	{
+		return str_replace(" ", "_", $this->name);
+	}
+
+	public function checkQuantityStatus()
+	{
+		if($this->useQuantity && $this->quantity == 0)
+		{
+			//$this->status = 
+		}
+	}
+	
+	public $id;
+	public $name;
+	public $shortDescription;
+	public $longDescription;
+	public $SKU;
+	public $price;
+	public $vatId;
+	public $manufacturerId;
+	public $weight;
+	public $status;
+	public $quantity;
+	public $dateAdded;
+	public $useQuantity;
+	
 }
