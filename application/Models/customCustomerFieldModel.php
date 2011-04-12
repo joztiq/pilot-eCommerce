a50308932e0ba6462daca5065fe7b1b56071a6ce
Markus Gerdau <markus.gerdau@gmail.com>
2011-04-12 15:16:56 +0200

    Work in progress :)

---
+++ application/Models/customCustomerFieldModel.php
@@ -0,0 +1,10 @@
+<?php
+class customCustomerFieldModel extends joz_activeRecordModel {
+	
+	public $tableName = 'pec_customCustomerFields';
+	
+	public $id;
+	public $name;
+	public $customerId;
+	
+}
---
+++ application/Models/customerModel.php
@@ -0,0 +1,28 @@
+<?php
+class customerModel extends joz_activeRecordModel {
+	
+	public $tableName = 'pec_customers';
+	
+	/*
+	* Markup for relations. You might want to add '@property' tags in the class phpDoc.
+	*/
+	protected $relations = array(
+				'customFields' => array(
+							'class' => 'customCustomerFieldModel',
+							'key' => 'customerId',
+							'type' => self::HAS_MANY),
+							);
+	
+	public $id;
+	public $firstName;
+	public $lastName;
+	public $streetAddress;
+	public $postalCode;
+	public $city;
+	public $phone;
+	public $country;
+	public $email;
+	public $password;
+	
+	
+}
